a9baa8885de7f52484dc9ae481f571f77658e68b
Erin Jackson <erinebjackson@gmail.com>
2022-01-12 10:30:30 -0800

    Adds paths and methods to display individual products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,21 @@ class ProductsController < ApplicationController
     render json:products.as_json
     
   end
+  def first_product
+    puts "What product would you like to see"
+    products = Product.first
+    render json:products.as_json
+  end
+  def last_product
+    products = Product.last
+    render json:products.as_json
+  end
+  def second_product
+    products = Product.all[1]
+    render json:products.as_json
+  end
+  def third_product
+    products = Product[2]
+    render json:products.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/products_all", controller: "products", action: "display_products"
+  
+  get "/first_product", controller: "products", action: "first_product"
+  
+  get "/last_product", controller: "products", action: "last_product"
+  
+  get "/second_product", controller: "products", action: "second_product"
+  
+  get "/third_product", controller: "products", action: "third_product"
 end
